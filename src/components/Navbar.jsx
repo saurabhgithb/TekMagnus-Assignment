c40f74502e18026c5c08e654159454506ccb72bb
saurabh-gih <saurav999bsr@gmail.com>
2024-01-04 18:28:43 +0530

    improved Search Bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { DarkMode, Mail, Notifications } from "@mui/icons-material";
+import { DarkMode, Mail, Notifications, SearchOutlined } from "@mui/icons-material";
 import {
   AppBar,
   Avatar,
@@ -21,11 +21,16 @@ const Navbar = () => {
     backgroundColor: "transparent",
   });
   const Search = styled("div")(({ theme }) => ({
-    backgroundColor: "white",
-    color: "black",
+    background:
+      "linear-gradient(180deg, rgba(37,36,44,0.6028536414565826) 0%, rgba(38,37,45,1) 95%)",
+    color: "white",
     padding: "0 10px",
-    borderRadius: theme.shape.borderRadius,
+    borderRadius: 10,
     width: "40%",
+    border: "1px solid white",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between"
   }));
   const Icons = styled(Box)(({ theme }) => ({
     display: "none",
@@ -37,31 +42,43 @@ const Navbar = () => {
   }));
   return (
     <Box sx={{ marginY: 2 }}>
-      <AppBar position="relative">
+      <AppBar
+        position="relative"
+        sx={{
+          width: "65rem",
+          maxWidth: "100%",
+          marginX: "auto",
+          background: "transparent",
+          boxShadow: "none",
+        }}
+      >
         <StyledToolbar>
           <Search>
-            <InputBase placeholder="search..." sx={{ color: "black", minWidth: "100%" }} />
+            <InputBase
+              placeholder="search..."
+            />
+            <SearchOutlined />
           </Search>
 
           <Icons>
-            <Button sx={{minWidth: "1px", padding: "10px 15px",}}>
+            <Button sx={{ minWidth: "1px", padding: "10px 15px" }}>
               <Badge variant="dot" color="error">
                 <Notifications />
               </Badge>
             </Button>
-            <Button sx={{minWidth: "1px", padding: "10px 15px",}}>
+            <Button sx={{ minWidth: "1px", padding: "10px 15px" }}>
               <Badge variant="dot" color="error">
                 <Mail />
               </Badge>
             </Button>
-            <Button sx={{minWidth: "1px", padding: "10px 15px",}}>
+            <Button sx={{ minWidth: "1px", padding: "10px 15px" }}>
               <DarkMode />
             </Button>
-            <Box sx={{cursor: "pointer"}}>
-            <Avatar
-              sx={{ width: 30, height: 30 }}
-              src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            />
+            <Box sx={{ cursor: "pointer" }}>
+              <Avatar
+                sx={{ width: 30, height: 30 }}
+                src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              />
             </Box>
           </Icons>
         </StyledToolbar>
