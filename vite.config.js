369c50b6b2527b4c27aa81be62ed349aeca3c6f7
saurabh-gih <saurav999bsr@gmail.com>
2024-01-04 17:16:53 +0530

    modified vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/saurabhgithb/TekMagnus-Assignment.git",
+  base: "/TekMagnus-Assignment.git",
   plugins: [react()],
 })
